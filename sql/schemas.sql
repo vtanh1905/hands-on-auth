0b536f535c464a5d71313a793487f1c3825882bc
Anh Vu Tuan <vtanh1905@gmail.com>
2024-07-14 23:09:48 +0700

    Update Schema

--- sql/schemas.sql
+++ sql/schemas.sql
@@ -1,19 +1,22 @@
+-- Create 'app' schema if it doesn't exist
 CREATE SCHEMA IF NOT EXISTS app;
 
+-- Create 'app.users' table
 CREATE TABLE IF NOT EXISTS app.users (
+   id SERIAL PRIMARY KEY,
    email VARCHAR(50),
-   password VARCHAR(255) NOT NULL,
-   PRIMARY KEY(email)
+   password VARCHAR(255) NOT NULL
 );
 
+-- Create 'app.video' table
 CREATE TABLE IF NOT EXISTS app.video (
    id VARCHAR(50),
-   email VARCHAR(50) NOT NULL,
+   user_id INTEGER NOT NULL,
    title TEXT,
    description TEXT,
    create_at TIMESTAMP NOT NULL,
    PRIMARY KEY(id),
    CONSTRAINT fk_video_to_users
-      FOREIGN KEY(email) 
-	      REFERENCES app.users(email)
+      FOREIGN KEY(user_id)
+         REFERENCES app.users(id)
 );
